141ba0adebde4eae86301bd0f558f24acb95f7c7
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2024-12-12 16:15:30 -0300

    Add loop for Kick and Current setpoints.
    Before this, the Kick-SP updates the Current-SP, but the inverse doesn't
    occurs. Now, the setpoints are always matching, no matter how was the
    one written. This is done to update Kick-SP when Current-SP is updated
    by someone else.

--- si_ps_conv_fastcorrsApp/src/strn2curr.py
+++ si_ps_conv_fastcorrsApp/src/strn2curr.py
@@ -14,12 +14,11 @@ class Converter(object):
         self.norm = NormalizerFactory.create(maname)
 
         self.dipole_strength = getRecord("copy-SI-Fam:PS-B1B2-1:EnergyRef-Mon")
-        self.base_record = getRecord("copy-" + parsed_args[0]+parsed_args[1])
+        self.base_record = getRecord("out-" + parsed_args[0]+parsed_args[1])
 
     def process(self, rec, reason):
         self.base_record.VAL = self.norm.conv_strength_2_current(
             rec.VAL, strengths_dipole=self.dipole_strength.VAL)
-        self.base_record.scan(sync=True)
 
     def detach(self, rec):
         pass
